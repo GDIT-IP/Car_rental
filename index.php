e2f77aeff32cbf100b6d5e8dc4f80ba901eeb9a1
Anton Rozhanskii <anton_rozhanskii@yahoo.com>
2020-02-29 13:13:50 +1300

    Change the way files organized
    It is going to become hard to maintain, name and understand
    what is what and where, and, especially, manage site's links,
    if the project has 'flat', 'linear' structure.
    
    Now the idea is we always address to index.php, but, depending on
    request's parameters different models and views are included.
    
    Video tutorial:
    https://youtu.be/PYB0mNMj5Lk

--- index.php
+++ index.php
@@ -1,64 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- Bootstrap -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <title>Car rental</title>
-    <!-- Custom styles -->
-    <link href="css/index.css" rel="stylesheet">
-</head>
-<body>
-<!-- Header -->
-<?php include 'components/header.php'?>
+<?php
+const DS = DIRECTORY_SEPARATOR;
+defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FILE__) . "."));
 
-<!-- Cars -->
-<div class="container mt-5">
-    <h3 class="mb-5">Our cars</h3>
-    <div class="row">
+require APPLICATION_PATH . DS . 'config' . DS . 'config.php';
 
-        <div class="d-flex flex-wrap">
-            <?php
-            for ($i = 0; $i < 8; $i++):
-                ?>
-                <div class="card mb-4 shadow-sm">
-                    <div class="card-header">
-                        <h4 class="my-0 font-weight-normal">$brand $model</h4>
-                    </div>
-                    <img class="img-fluid" src="img/car_<?php echo($i + 1) ?>.jpg" alt="">
-                    <div class="card-body">
-                        <h1 class="card-title pricing-card-title">$price <small class="text-muted">/ day</small></h1>
-                        <ul class="list-unstyled mt-3 mb-4">
-                            <li>10 users included</li>
-                            <li>2 GB of storage</li>
-                            <li>Email support</li>
-                            <li>Help center access</li>
-                        </ul>
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                        </div>
-                    </div>
-                </div>
-            <?php endfor; ?>
-        </div>
-    </div>
-</div>
+$page = get('page');
+$model = $config['MODEL_PATH'] . $page . '.php';
+$view = $config['VIEW_PATH'] . $page . '.php';
 
-<!-- Footer -->
-<?php include 'components/footer.php'?>
+console_log($model);
+console_log($view);
 
-</body>
-<!-- Bootstrap -->
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"
-        integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm"
-        crossorigin="anonymous"></script>
+if (file_exists($model)) {
+    require $model;
+}
 
-</html>
+if (file_exists($view)) {
+    require $view;
+}
+
+?>
---
+++ view/singup.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Bootstrap -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Car rental</title>
+    <!-- Custom styles -->
+    <link href="../css/registration.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+<div class="container mt-6">
+    <h1>Create new account</h1>
+    <form action="../check_reg.php" method="POST">
+        <div class="row">
+            <span class="mr-2">*</span>
+            <input type="text" class="col form-control" name="login" id="login" placeholder="Enter login"
+                   onfocusout="checkLogin()">
+            <label class="col" id="loginWarning" required=""></label>
+        </div>
+        <br>
+        <div class="row">
+            <span class="mr-2">*</span>
+            <input type="password" class="col form-control" name="password" id="password" placeholder="Enter password"
+                   onfocusout="checkPasswordLength()" required="">
+            <label class="col" id="passWarning"></label>
+        </div>
+        <br>
+        <div class="row">
+            <span class="mr-2">*</span>
+            <input type="password" class="col form-control" name="confirm-password" id="confirm-password"
+                   placeholder="Confirm password">
+            <label class="col" id="confirmPassWarning"></label>
+        </div>
+        <br>
+        <input type="email" class="form-control" name="email" id="email" placeholder="Enter email"
+               oninput="validateValue(this)"><br>
+        <input type="text" class="form-control" name="first-name" id="first-name" placeholder="Enter First name"
+               oninput="validateValue(this)"><br>
+        <input type="text" class="form-control" name="last-name" id="last-name" placeholder="Enter Last name"
+               oninput="validateValue(this)"><br>
+        <button type="submit" class="btn btn-success">Create account</button>
+    </form>
+</div>
+</body>
+<script src="../js/validations/singup.js"></script>
+</html>

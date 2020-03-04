28aa69d471f1dcc43b4d1d1a0af82f8b0d84367c
Juan Fernando Parra <jparrax17@gmail.com>
2020-03-05 12:45:40 +1300

    Style Change in footer, header and login
    The footer was modified to eliminate from class pt duplication, and md is not working so I removed it.
    
    In header I include a hyperlink to home in Company Name title and in our cars.
    
    Login container class was modified to center the text and input password class was modified to center "forgot password?".

--- view/footer.php
+++ view/footer.php
@@ -1,5 +1,4 @@
-<!-- Move footer to the bottom -->
-<footer class="container pt-4 my-md-5 pt-md-5 border-top">
+<footer class="container pt-4 my-5 border-top">
     <div class="row">
         <div class="col-12 col-md">
             <small class="d-block mb-3 text-muted">© 2017-2019</small>

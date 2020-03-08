d5f2de263de5e20cda78ef7cd6cbb0e82a7d2262
Anton Rozhanskii <anton_rozhanskii@yahoo.com>
2020-03-08 19:40:09 +1300

    Format code a bit (no functional changes)

--- view/login.php
+++ view/login.php
@@ -1,12 +1,13 @@
 <div class="container-fluid my-container">
     <div class="d-flex justify-content-center">
         <form class="col-md-7 col-lg-4" action="" method="post">
-            <input type="text" id="login" name="login" class="form-control mb-2" placeholder="Enter user name" autofocus="">
+            <input type="text" id="login" name="login" class="form-control mb-2" placeholder="Enter user name"
+                   autofocus="">
             <input type="password" id="password" name="password" class="form-control mb-2" placeholder="Enter password">
             <a href="#">Forgot password?</a>
             <input class="btn btn-lg btn-primary btn-block my-3" type="submit" value="Sign In">
-            <div class = "container" id ="sign-up-container">
-                <a href="?page=signup" id = "sign-up">Sign Up</a>
+            <div class="container" id="sign-up-container">
+                <a href="?page=signup" id="sign-up">Sign Up</a>
             </div>
         </form>
     </div>

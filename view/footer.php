8f53909acc0ca05c397ab6c1a9e81951df5685d4
Juan Fernando Parra <jparrax17@gmail.com>
2020-04-03 09:12:36 +1300

    Minor change on footer

--- view/footer.php
+++ view/footer.php
@@ -3,16 +3,16 @@
         <div class="col-12 col-md">
             <small class="d-block mb-3 text-muted">© 2017-2019</small>
         </div>
-        <div class="col-6 col-md">
+        <!-- <div class="col-6 col-md">
             <h5>Features</h5>
             <ul class="list-unstyled text-small">
                 <li><a class="text-muted" href="#">Cool stuff</a></li>
             </ul>
-        </div>
+        </div> -->
         <div class="col-6 col-md">
             <h5>Resources</h5>
             <ul class="list-unstyled text-small">
-                <li><a class="text-muted" href="/?page=feedback">Enquiry and Feedbacks</a></li>
+                <li><a class="text-muted" href="/?page=feedback">Enquiries and Feedbacks</a></li>
             </ul>
         </div>
         <div class="col-6 col-md">

3268f2c741f4f67b95754670d91d1419ff6fb522
Anton Rozhanskii <anton_rozhanskii@yahoo.com>
2020-04-02 13:38:57 +1300

    "Add user" button must be bellow the table

--- view/usersManagment.php
+++ view/usersManagment.php
@@ -1,9 +1,4 @@
-<div class="row">
-    <div class="col mb-4">
-        <a class="btn btn-primary" href="?page=signup">Add user</a>
-    </div>
-</div>
-<table class="table">
+<table class="table mt-5">
     <thead>
     <tr>
         <th scope="col">Id</th>
@@ -26,6 +21,12 @@
         </ul>
     </nav>
 <?php } ?>
+
+<div class="row">
+    <div class="col mb-4">
+        <a class="btn btn-primary" href="?page=signup">Add user</a>
+    </div>
+</div>
 
 <script type="text/javascript">let usersPerPage = <?= $usersPerPage; ?></script>
 <script type="text/javascript" src="./js/usersManagement.js"></script>

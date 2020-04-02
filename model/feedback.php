14b8ee319b75c7358b40bd83ead6d1a9525c81cb
Juan Fernando Parra <jparrax17@gmail.com>
2020-04-03 11:57:24 +1300

    Minor changes

--- model/feedback.php
+++ model/feedback.php
@@ -8,6 +8,7 @@ require 'PHPMailer/PHPMailer.php';
 require 'PHPMailer/SMTP.php';
 
 $mail = new PHPMailer(true);
+$mailer = getMailer();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $fname = $lname = $email = $msg = '';
@@ -27,16 +28,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //Server settings
     // $mail->SMTPDebug = 1;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->Host       = $mailer->smtpHost;                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'a2i.chinchilla.mailer@gmail.com';                     // SMTP username
-    $mail->Password   = 'a2i_chinchilla';                               // SMTP password
+    $mail->Username   = $mailer->smtpUsername;                     // SMTP username
+    $mail->Password   = $mailer->smtpPassword;;                               // SMTP password
     $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('a2i.chinchilla.mailer@gmail.com', 'Enquiry or feedbacks');
-    $mail->addAddress('a2i.chinchilla.mailer@gmail.com', 'Enquiry or feedbacks');     // Add a recipient
+    $mail->setFrom($mailer->smtpUsername, 'Enquiry or feedbacks');
+    $mail->addAddress($mailer->smtpUsername, 'Enquiry or feedbacks');     // Add a recipient
 
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
